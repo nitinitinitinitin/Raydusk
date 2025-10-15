76b45788b66e930d12e4bade27c6fa9c96dbfde4
dbzprime <primedbz7097@gmail.com>
2025-10-15 16:45:12 +0530

    adding 2d and 3d viewports with splitter

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,3 +13,9 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_centralwidget_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_centralwidget_customContextMenuRequested(const QPoint &pos);
+
 private:
     Ui::MainWindow *ui;
 };
